ad6a5668038a0b15bc7beac34f2b9d9eab4468c9
anthony <anthony@gmail.com>
2026-07-04 15:57:50 -0600

    direccion service

---
+++ app/services/cliente_servicio_service.py
@@ -0,0 +1,83 @@
+from sqlalchemy.orm import Session
+
+from app.models.clientes_servicios import ClienteServicio
+from app.repositories.cliente_servicio_repository import ClienteServicioRepository
+from app.schemas.cliente_servicio_dto import ClienteServicioCreateDTO, ClienteServicioResponseDTO, ClienteServicioUpdateDTO
+
+class ClienteServicioService:
+    
+    def create_cliente_servicio(
+            self,
+            db: Session,
+            cliente_servicio_in: ClienteServicioCreateDTO,
+    ) -> ClienteServicioResponseDTO:
+
+        cliente_servicio_db = ClienteServicio(
+            id_cliente= cliente_servicio_in.id_cliente,
+            id_servicio= cliente_servicio_in.id_servicio,
+            fecha_adquisicion= cliente_servicio_in.fecha_adquisicion
+        )
+
+        cliente_servicio_creado = ClienteServicioRepository.create_cliente_servicio(db, cliente_servicio_db)
+
+        return ClienteServicioResponseDTO.model_validate(cliente_servicio_creado)
+    
+    # ── READ (uno solo) ─────────────────────────────────────────────
+    def get_cliente_servicio_by_id(
+            self,
+            db: Session,
+            id_cliente_servicio: int,
+    ) -> ClienteServicioResponseDTO | None:
+
+        cliente_servicio = ClienteServicioRepository.get_cliente_servicio_by_id(db, id_cliente_servicio)
+
+        if cliente_servicio is None:
+            return None
+        return ClienteServicioResponseDTO.model_validate(cliente_servicio)
+    
+    def get_list_clientes_servicios(
+            self,
+            db: Session,
+    ) -> list[ClienteServicioResponseDTO]:
+
+        clientes_servicios = ClienteServicioRepository.get_list_clientes_servicios(db)
+
+        return [ClienteServicioResponseDTO.model_validate(cliente_servicio) for cliente_servicio in clientes_servicios]
+    
+    def update_cliente_servicio(
+            self,
+            db: Session,
+            id_cliente_servicio: int,
+            cliente_servicio_in: ClienteServicioUpdateDTO,
+    ) -> ClienteServicioResponseDTO | None:
+
+        cliente_servicio_db = ClienteServicioRepository.get_cliente_servicio_by_id(db, id_cliente_servicio)
+
+        if cliente_servicio_db is None:
+            return None
+
+        # Actualizar los campos del objeto cliente_servicio_db con los datos del DTO de entrada
+        for field, value in cliente_servicio_in.model_dump(exclude_unset=True).items():
+            setattr(cliente_servicio_db, field, value)
+
+        # Guardar los cambios en la base de datos
+        cliente_servicio_actualizado = ClienteServicioRepository.update_cliente_servicio(db, cliente_servicio_db)
+
+        return ClienteServicioResponseDTO.model_validate(cliente_servicio_actualizado)
+    
+    def delete_cliente_servicio(
+            self,
+            db: Session,
+            id_cliente_servicio: int,
+    ) -> bool:
+
+        cliente_servicio_db = ClienteServicioRepository.get_cliente_servicio_by_id(db, id_cliente_servicio)
+
+        if cliente_servicio_db is None:
+            return False
+
+        ClienteServicioRepository.delete_cliente_servicio(db, cliente_servicio_db)
+
+        return True 
+
+cliente_servicio_service = ClienteServicioService()
---
+++ app/services/departament_service.py
@@ -0,0 +1,82 @@
+from sqlalchemy.orm import Session
+
+from app.models.departamentos import Departamento
+from app.repositories.departamento_repository import DepartamentoRepository
+from app.schemas.departamento_dto import DepartamentoCreateDTO, DepartamentoResponseDTO, DepartamentoUpdateDTO
+
+class DepartamentoService:
+    
+    def create_departamento(
+            self,
+            db: Session,
+            departamento_in: DepartamentoCreateDTO,
+    ) -> DepartamentoResponseDTO:
+
+        departamento_db = Departamento(
+            nombre_departamento= departamento_in.nombre_departamento
+        )
+
+        departamento_creado = DepartamentoRepository.create_departamento(db, departamento_db)
+
+        return DepartamentoResponseDTO.model_validate(departamento_creado)
+    
+    # ── READ (uno solo) ─────────────────────────────────────────────
+    def get_departamento_by_id(
+            self,
+            db: Session,
+            id_departamento: int,
+    ) -> DepartamentoResponseDTO | None:
+
+        departamento = DepartamentoRepository.get_departamento_by_id(db, id_departamento)
+
+        if departamento is None:
+            return None
+        return DepartamentoResponseDTO.model_validate(departamento)
+    
+    def get_list_departamentos(
+            self,
+            db: Session,
+    ) -> list[DepartamentoResponseDTO]:
+
+        departamentos = DepartamentoRepository.get_list_departamentos(db)
+
+        return [DepartamentoResponseDTO.model_validate(departamento) for departamento in departamentos]
+    
+    def update_departamento(
+            self,
+            db: Session,
+            id_departamento: int,
+            departamento_in: DepartamentoUpdateDTO,
+    ) -> DepartamentoResponseDTO | None:
+
+        departamento_db = DepartamentoRepository.get_departamento_by_id(db, id_departamento)
+
+        if departamento_db is None:
+            return None
+
+        # Actualizar los campos del objeto departamento_db con los datos del DTO de entrada
+        for field, value in departamento_in.model_dump(exclude_unset=True).items():
+            setattr(departamento_db, field, value)
+
+        # Delegar la actualización al repositorio (commit)
+        departamento_actualizado = DepartamentoRepository.update_departamento(db, departamento_db)
+
+        return DepartamentoResponseDTO.model_validate(departamento_actualizado)
+    
+    def delete_departamento(
+            self,
+            db: Session,
+            id_departamento: int,
+    ) -> bool:
+
+        departamento_db = DepartamentoRepository.get_departamento_by_id(db, id_departamento)
+
+        if departamento_db is None:
+            return False
+
+        DepartamentoRepository.delete_departamento(db, departamento_db)
+        return True
+    
+
+#instancia
+departamento_service = DepartamentoService()1
