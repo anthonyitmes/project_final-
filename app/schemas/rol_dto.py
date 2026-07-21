34ddf944e3df543fc429af4293f8b114eba5e5ae
anthony <anthony@gmail.com>
2026-07-20 20:21:48 -0600

    Actualizar schemas: rol_dto, ticket_dto

--- app/schemas/rol_dto.py
+++ app/schemas/rol_dto.py
@@ -5,13 +5,13 @@ from pydantic import BaseModel
 
 class RolCreateDTO(BaseModel):
 	nombre_rol: str
-	descripcion_rol: Optional[str] = None
+	descripcion: Optional[str] = None
 
 
 class RolResponseDTO(BaseModel):
 	id_rol: int
 	nombre_rol: str
-	descripcion_rol: Optional[str]
+	descripcion: Optional[str]
 
 	model_config = {
 		"from_attributes": True
@@ -20,4 +20,4 @@ class RolResponseDTO(BaseModel):
 
 class RolUpdateDTO(BaseModel):
 	nombre_rol: Optional[str] = None
-	descripcion_rol: Optional[str] = None
+	descripcion: Optional[str] = None
